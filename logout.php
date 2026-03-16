314a0273e1b41fce7254f806cfafd98cdb7c4557
RomitoLorenzo <romito.lorenzo@itisriva.edu.it>
2026-03-16 13:59:19 +0100

    reset

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_destroy();
-?>
-<html>
-    <p>Vuoi disconnetterti?</p>
-    <a class="btn btn-primary" href="login.php" role="button">Sì</a>
-    <a class="btn btn-secondary" href="index.php" role="button">No</a>
-</html>
