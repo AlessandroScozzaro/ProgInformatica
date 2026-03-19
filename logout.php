640ec3507de0f4ff29680d277e30c2ec3aab8eea
RomitoLorenzo <romito.lorenzo@itisriva.edu.it>
2026-03-19 13:35:57 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,6 +3,6 @@ session_start()
 ?>
 <html>
     <p>Vuoi disconnetterti?</p>
-    <button href="login.php" role="button">OK</button>
-    <button href="index.php" role="button">Cancel</button>
+    <button onclick="window.location.href='login.php'" role="button">OK</button>
+    <button onclick="window.location.href='index.php'" role="button">Cancel</button>
 </html>
