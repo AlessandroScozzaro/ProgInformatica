5effac9ce8980f2524818b6770837cc23b64e33a
wadiijbili <jbili.wadii.21@itisriva.edu.it>
2026-03-19 12:45:16 +0100

    identifica ruolo

--- auth.php
+++ auth.php
@@ -3,15 +3,19 @@ session_start();
 require_once 'lib/conn.php';
 $email = $_POST['email'];
 $password = $_POST['password'];
-$stmt = $conn->prepare("SELECT id FROM utenti WHERE email = :email AND password = :password");
+$stmt = $conn->prepare("SELECT id, ruolo FROM utenti WHERE email = :email AND password = :password");
 $stmt->bindParam(':email', $email);
 $stmt->bindParam(':password', $password);
 $stmt->execute();
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$row = $stmt->fetch();
 if($stmt->rowCount() > 0){
-    $_SESSION['id'] = $stmt->fetch()['id'];
+    $_SESSION['id'] = $row['id'];
+    $_SESSION['ruolo'] = $row['ruolo'];
     header("Location: index.php");
     exit();
 } else {
     header("Location: login.php");
     exit();
 }
+?>
