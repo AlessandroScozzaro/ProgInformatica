24260ceec5460508a401ea01743567bc5f2e28fc
RomitoLorenzo <romito.lorenzo@itisriva.edu.it>
2026-03-19 13:01:51 +0100

    auth.php adjustments

--- auth.php
+++ auth.php
@@ -3,15 +3,14 @@ session_start();
 require_once 'lib/conn.php';
 $email = $_POST['email'];
 $password = $_POST['password'];
-$stmt = $conn->prepare("SELECT id, ruolo FROM utenti WHERE email = :email AND password = :password");
+$stmt = $conn->prepare("SELECT id_utente, ruolo FROM utenti WHERE email = :email AND password = :password");
 $stmt->bindParam(':email', $email);
 $stmt->bindParam(':password', $password);
 $stmt->execute();
 $stmt->setFetchMode(PDO::FETCH_ASSOC);
-$row = $stmt->fetch();
 if($stmt->rowCount() > 0){
-    $_SESSION['id'] = $row['id'];
-    $_SESSION['ruolo'] = $row['ruolo'];
+    $_SESSION['id'] = $stmt->fetch()['id_utente'];
+    $_SESSION['ruolo'] = $stmt->fetch()['ruolo'];
     header("Location: index.php");
     exit();
 } else {
