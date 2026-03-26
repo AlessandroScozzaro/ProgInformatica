498d582579ad90216c4f9877400b7746ea09fb16
Lor110 <orlando.lorenzo.21@itisriva.edu.it>
2026-03-26 13:13:31 +0100

    riupdate utenti.php

--- utenti.php
+++ utenti.php
@@ -111,16 +111,10 @@ try {
                 </h5>
 
                 <ul class="navbar-nav ml-auto">
-<<<<<<< Updated upstream
-                    <li class="nav-item dropdown no-arrow">
-                        <a class="nav-link dropdown-toggle" href="#">
-                            <a href="logout.php"><span class="mr-2 d-none d-lg-inline text-gray-600 small">Utente</span></a>
-                            <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
-                        </a>
-=======
+
                     <li class="nav-item">
                         <a href="logout.php" class="btn btn-sm btn-danger">Logout</a>
->>>>>>> Stashed changes
+
                     </li>
                 </ul>
             </nav>
